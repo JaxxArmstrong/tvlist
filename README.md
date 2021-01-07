7297c755adcd9296272f17d746a3df16f9cd16ca
damianrath <13659704+damianrath@users.noreply.github.com>
2021-01-07 05:07:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# tvlist
+Lists current state of TV shows from either a predefined list (textfile), TV shows defined at the command line or search for a specific TV show. All modes leverages the TV Maze API.
+
+UPDATE (2019-11-07):  Recompiled the *Linux* binary (go version go1.13.3 linux/amd64) just to make sure it still works as advertised including checking the TV Maze API functionality overall.
+
+![](images/tvlist1.png?raw=true)
+![](images/tvlist2.png?raw=true)
+![](images/tvlist3.png?raw=true)
+
+This personal project is published as-is and offers no support beyond this README.
+
+It's written in [Go](https://golang.org/) and is part of my effort to learn the specific syntax by a learn-by-doing approach. The code is not elegant, but it's working as intended. I will polish it when time allows for it.
+
+I've supplied a pre-compiled *Linux* standalone binary along with the source code for you who just want to clone the repo and have it working right off the bat, without installing *Go* as a language on your workstation.
+
+## How it works
+
+This utility fetches properties for predefined TV show from the JSON API backend at [TV Maze](http://www.tvmaze.com/). These are then shown in a short and aligned list in your terminal so you can get a quick overview of the TV shows you're following. It can also be used to search for a specific TV show (with more properties shown) instead of listing predefined TV shows.
+
+If you want to use the utility to iterate through a textfile that contains TV show IDs you need to first create that file on your own and populate it with the appropriate unique TV Show ID found at TV Maze. See the below image for an example.
+
+If filename is omitted on the command line for the list mode the utility will check for a default file in the following places:
+
+- ~/Documents/tvshows
+- ~/.tvshows
+- ~/tvshows
+
+![](images/tvlist4.png?raw=true)
+
+Note that **only** the first column is extracted by the utility and the remaining text to the right of the TV Maze ID is just for comments.
+
+The unique TV Maze ID can easily be fetched with the *search-mode* of this utility. Note that the TV Maze API tries to guess the correct TV show if you supply it with one or more keywords. If you need to use more than one keyword, please enclose it in quotes. See *usage* below.
+
+The ID can also be gathered by using the API directly in a webbrowser. Note that sometimes more than one show has the same name so check the JSON result of the search to make sure you have the correct show. [Example search](http://api.tvmaze.com/singlesearch/shows?q=girls)
+
+
+## Usage
+
+```
+tvlist list file [<textfile>]
+tvlist list id 32819,12607,8898
+tvlist search 'marvels agents'
+tvlist search billions
+```
+
+I intentionally kept the commands & the arguments in a longer format for ease of understanding. If you don't agree with them you can either create an alias to your preferred shorter format or edit the source code and re-compile the executable.
+
+
+If you have _Go_ installed as a language and your _$GOPATH_ set, you can just pull it with:
+```
+go get github.com/jaxxarmstrong/tvlist
+```
+
+---
+
+## Credits
+
+A big thank you to [gotbletu](https://www.youtube.com/channel/UCkf4VIqu3Acnfzuk3kRIFwA) for helping out with testing and recommending features.
---
+++ tvlist.go
@@ -0,0 +1,369 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/user"
+	"path/filepath"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/Guitarbum722/align"
+	"github.com/denisbrodbeck/striphtmltags"
+	"github.com/docopt/docopt-go"
+)
+
+var searchCMD bool
+var searchArgs string
+var listCMD bool
+var listModeID bool
+var listModeIDArgs string
+var listModeFile bool
+var listModeFileArgs string
+var lines []string
+var url string
+var list string
+var mytvmaze1 = MyTVmaze{}
+var mytvmaze2 = MyTVmazeDates{}
+
+var myFile = []string{"~/Documents/tvshows", "~/.tvshows", "~/tvshows"}
+
+type MyTVmaze struct {
+	ID           int      `json:"id"`
+	URL          string   `json:"url"`
+	Name         string   `json:"name"`
+	Type         string   `json:"type"`
+	Language     string   `json:"language"`
+	Genres       []string `json:"genres"`
+	Status       string   `json:"status"`
+	Runtime      int      `json:"runtime"`
+	Premiered    string   `json:"premiered"`
+	OfficialSite string   `json:"officialSite"`
+	Schedule     struct {
+		Time string   `json:"time"`
+		Days []string `json:"days"`
+	} `json:"schedule"`
+	Rating struct {
+		Average float64 `json:"average"`
+	} `json:"rating"`
+	Weight     int         `json:"weight"`
+	Network    interface{} `json:"network"`
+	WebChannel struct {
+		ID      int    `json:"id"`
+		Name    string `json:"name"`
+		Country struct {
+			Name     string `json:"name"`
+			Code     string `json:"code"`
+			Timezone string `json:"timezone"`
+		} `json:"country"`
+	} `json:"webChannel"`
+	Externals struct {
+		Tvrage  interface{} `json:"tvrage"`
+		Thetvdb int         `json:"thetvdb"`
+		Imdb    string      `json:"imdb"`
+	} `json:"externals"`
+	Image struct {
+		Medium   string `json:"medium"`
+		Original string `json:"original"`
+	} `json:"image"`
+	Summary string `json:"summary"`
+	Updated int    `json:"updated"`
+	Links   struct {
+		Self struct {
+			Href string `json:"href"`
+		} `json:"self"`
+		Previousepisode struct {
+			Href string `json:"href"`
+		} `json:"previousepisode"`
+		Nextepisode struct {
+			Href string `json:"href"`
+		} `json:"nextepisode"`
+	} `json:"_links"`
+}
+
+type MyTVmazeDates struct {
+	ID       int       `json:"id"`
+	URL      string    `json:"url"`
+	Name     string    `json:"name"`
+	Season   int       `json:"season"`
+	Number   int       `json:"number"`
+	Airdate  string    `json:"airdate"`
+	Airtime  string    `json:"airtime"`
+	Airstamp time.Time `json:"airstamp"`
+	Runtime  int       `json:"runtime"`
+	Image    struct {
+		Medium   string `json:"medium"`
+		Original string `json:"original"`
+	} `json:"image"`
+	Summary interface{} `json:"summary"`
+	Links   struct {
+		Self struct {
+			Href string `json:"href"`
+		} `json:"self"`
+	} `json:"_links"`
+}
+
+func printOutput(showList []string) {
+
+	var showListExploded = ""
+
+	sort.Strings(showList)
+
+	for _, line := range showList {
+		showListExploded = showListExploded + line + "\n"
+	}
+
+	input := strings.NewReader(showListExploded)
+	output := bytes.NewBufferString("")
+	q := align.TextQualifier{On: true, Qualifier: "\""}
+	aligner := align.NewAlign(input, output, "|", q)
+	aligner.OutputSep("/")
+	aligner.Align()
+
+	fmt.Println(output)
+
+	return
+}
+
+func doFileExist(contraband string) bool {
+	if _, err := os.Stat(contraband); err == nil {
+		// path/to/whatever does exists
+		return true
+	}
+	// the path does not exist or some error occurred.
+	return false
+}
+
+func getTVshows(url string) {
+
+	tvmazeClient := http.Client{
+		Timeout: time.Second * 3, // Maximum of 3 secs
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0")
+
+	res, getErr := tvmazeClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	mytvmaze1 = MyTVmaze{}
+
+	jsonErr := json.Unmarshal(body, &mytvmaze1)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	return
+}
+
+func getTVshowsAirdate(url string) string {
+
+	tvmazeClient2 := http.Client{
+		Timeout: time.Second * 3, // Maximum of 3 secs
+	}
+
+	req2, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req2.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0")
+
+	res2, getErr := tvmazeClient2.Do(req2)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	body2, readErr := ioutil.ReadAll(res2.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	mytvmaze2 = MyTVmazeDates{}
+
+	jsonErr2 := json.Unmarshal(body2, &mytvmaze2)
+	if jsonErr2 != nil {
+		log.Fatal(jsonErr2)
+	}
+
+	return mytvmaze2.Airdate
+}
+
+func gatherInfo(url string) (string, string) {
+	nextep := ""
+	prevep := ""
+
+	getTVshows(url)
+
+	if mytvmaze1.Links.Nextepisode.Href != "" {
+		nextep = getTVshowsAirdate(mytvmaze1.Links.Nextepisode.Href)
+	} else {
+		nextep = "N/A"
+	}
+
+	if mytvmaze1.Links.Previousepisode.Href != "" {
+		prevep = getTVshowsAirdate(mytvmaze1.Links.Previousepisode.Href)
+	} else {
+		prevep = "N/A"
+	}
+
+	return nextep, prevep
+}
+
+func main() {
+	fmt.Println()
+
+	usage := `tvlist v0.6.1
+
+Usage:
+	tvlist search (<name>)
+	tvlist list file [<filename>]
+	tvlist list id (<id>)
+	tvlist -h | --help
+
+Options:
+	-h --help   Show this screen.
+	id          ID mode.
+	<id>        ID(s) of the TV shows to list.
+	file        file mode.
+	<filename>  File to parse for TV shows.`
+
+	args, _ := docopt.ParseDoc(usage)
+
+	searchCMD, _ = args.Bool("search")
+	searchArgs, _ = args.String("<name>")
+	listCMD, _ = args.Bool("list")
+	listModeID, _ = args.Bool("id")
+	listModeIDArgs, _ = args.String("<id>")
+	listModeFile, _ = args.Bool("file")
+	listModeFileArgs, _ = args.String("<filename>")
+
+	nextep := ""
+	prevep := ""
+
+	if listCMD {
+		if listModeFile {
+			usr, _ := user.Current()
+			dir := usr.HomeDir
+
+			if listModeFileArgs == "" {
+				for _, i := range myFile {
+
+					// golang is annoying so we need to expand ~
+					if strings.HasPrefix(i, "~/") {
+						i = filepath.Join(dir, i[2:])
+					}
+
+					// Does a default file exist?
+					foundYou := doFileExist(i)
+					if foundYou == true {
+						listModeFileArgs = i
+						break
+					} else {
+						// No default file found so flip the user the bird.
+						fmt.Println("No default file nor a file given at the command line, exiting.")
+						os.Exit(1)
+					}
+				}
+			}
+
+			file, err := os.Open(listModeFileArgs)
+			if err != nil {
+				fmt.Println()
+				log.Fatal(err)
+			}
+
+			fmt.Printf("Working")
+
+			fscanner := bufio.NewScanner(file)
+
+			for fscanner.Scan() {
+				fmt.Printf(".")
+
+				myString := fscanner.Text()
+
+				s := strings.Split(myString, " ")[0]
+				url = "http://api.tvmaze.com/shows/" + s
+
+				nextep, prevep = gatherInfo(url)
+				lines = append(lines, nextep+"|"+prevep+"|"+mytvmaze1.Name+"|"+mytvmaze1.Status)
+			}
+
+			defer file.Close()
+
+		} else {
+			s := strings.Split(listModeIDArgs, ",")
+
+			fmt.Printf("Working")
+
+			for i := range s {
+				fmt.Printf(".")
+
+				url = "http://api.tvmaze.com/shows/" + s[i]
+
+				nextep, prevep = gatherInfo(url)
+				lines = append(lines, nextep+"|"+prevep+"|"+mytvmaze1.Name+"|"+mytvmaze1.Status)
+			}
+		}
+
+		fmt.Printf("\n\n")
+
+		printOutput(lines)
+	}
+
+	if searchCMD {
+		url = "http://api.tvmaze.com/singlesearch/shows?q=" + searchArgs
+
+		nextep, prevep = gatherInfo(url)
+
+		// fmt.Println()
+		fmt.Println("ID:", mytvmaze1.ID)
+		fmt.Println("Name:", mytvmaze1.Name)
+		fmt.Println("Status:", mytvmaze1.Status)
+		fmt.Println("Rating:", mytvmaze1.Rating.Average)
+
+		fmt.Printf("Genres: ")
+
+		for i := range mytvmaze1.Genres {
+			fmt.Printf(mytvmaze1.Genres[i])
+			fmt.Printf(" ")
+		}
+
+		fmt.Println("\nLanguage:", mytvmaze1.Language)
+
+		fmt.Println("Network:", mytvmaze1.WebChannel.Name)
+
+		fmt.Println("Premiered:", mytvmaze1.Premiered)
+		fmt.Println("URL:", mytvmaze1.URL)
+
+		fmt.Println("---")
+
+		mySummary := striphtmltags.StripTags(mytvmaze1.Summary)
+		fmt.Println("Summary:", mySummary)
+
+		fmt.Println("---")
+
+		fmt.Println("Previous episode:", prevep)
+		fmt.Println("Next episode:", nextep)
+	}
+
+	os.Exit(0)
+}
